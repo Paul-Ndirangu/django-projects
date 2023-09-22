23e3ab1c42ce8ad39467e6fdf1dc9abb9479934e
Paul-Ndirangu <paulmwaura254@gmail.com>
2023-09-22 12:27:22 +0300

    templates added

--- blog_app/blog/urls.py
+++ blog_app/blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:post_id>/', views.post, name='post'),
 ]
  
--- blog_app/blog/views.py
+++ blog_app/blog/views.py
@@ -1,7 +1,18 @@
 from django.http import HttpResponse
+from django.template import loader
+from .models import Post
 
 # Create your views here.
 #
 def index(request):
-    return HttpResponse("Welcome to home page!")
+    latest_post_list = Post.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('blog/index.html')
+    context = {
+        'latest_post_list': latest_post_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def post(request, post_id):
+    return HttpResponse("You're looking at post %s." % post_id)
 
