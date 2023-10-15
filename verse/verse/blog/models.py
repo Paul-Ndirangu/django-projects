b22e0f641824b093f43bbb2ccfdc7b02b9677725
Paul-Ndirangu <paulmwaura254@gmail.com>
2023-10-15 09:03:17 +0300

    commit views and url patterns

--- verse/verse/blog/models.py
+++ verse/verse/blog/models.py
@@ -3,6 +3,12 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+
+# creating model manager
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager,self).get_queryset().filter(status='published')
+ 
 # post model
 class Post(models.Model):
     STATUS_CHOICES = (
@@ -20,9 +26,13 @@ class Post(models.Model):
     updated = models.DateTimeField(auto_now=True)
     
     status = models.CharField(max_length=10,choices=STATUS_CHOICES,default='draft')
-
+    
     class Meta:
         ordering = ('-publish',)
     
     def __str__(self):
         return self.title
+    
+    objects = models.Manager() # The default manager.
+    published = PublishedManager() # Our custom manager.
+
--- verse/verse/blog/views.py
+++ verse/verse/blog/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
-# Create your views here.
+
+def post_detail(request, post):
+    post=get_object_or_404(Post,slug=post,status='published')
+    return render(request, 'post_detail.html',{'post':post})
+
+def post_list(request):
+    posts = Post.published.all()
+    return render(request,'post_list.html',{'posts':posts})
