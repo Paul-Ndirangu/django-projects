b6553b45b9e9797db7ae9d1d2925e97c703cb48e
Paul-Ndirangu <paulmwaura254@gmail.com>
2024-01-27 17:37:56 +0300

    .

--- todo-app/todo/models.py
+++ todo-app/todo/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+# Models.
+class Task(models.Model):
+    class StatusChoice(models.TextChoices):
+        TODO = "To Do"
+        DOING = "Doing"
+        DONE = "done"
+        
+    name = models.CharField(max_length=255)
+    created = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(
+        choices = StatusChoice.choices, max_length=100, default=StatusChoice.TODO
+        )
+    
+    def __str__(self):
+        """
+        Provides a readable reference for each object
+        """
+        return f"{self.status}: {self.name}"
+    
