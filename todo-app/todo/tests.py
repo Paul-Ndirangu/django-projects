b6f2639ac61693fa81435e7e45e4a7773123fc42
Paul-Ndirangu <paulmwaura254@gmail.com>
2024-01-27 17:50:40 +0300

    test file commit

--- todo-app/todo/tests.py
+++ todo-app/todo/tests.py
@@ -1,3 +1,37 @@
 from django.test import TestCase
+import datetime
+from todo.models import Task
 
-# Create your tests here.
+# App tests.
+
+class TestTaskModel(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        """Creates a task instance that our tests can access.
+        setUpTestData is executed once, so our task instance is
+        shared between tests.
+        """
+        cls.task_name = "Learn Docker"
+        cls.task = Task.objects.create(name=cls.task_name)
+        
+    def test_task_name(self):
+        self.assertEqual(self.task.name, self.task_name)
+        
+    def test_task_default_status(self):
+        """Test that our task was assigned a status of 'To Do'"""
+        
+        expected = Task.StatusChoice.TODO
+        actual = self.task.status
+        
+        self.assertEqual(expected, actual)
+        
+    def test_task_created(self):
+        """Tests that task.created stores a datetime"""
+ 
+        self.assertEqual(type(self.task.created), datetime.datetime)
+        
+    def test_str(self):
+        expected = f"To Do: {self.task_name}"
+        actual = str(self.task)
+        
+        self.assertEqual(expected, actual)
