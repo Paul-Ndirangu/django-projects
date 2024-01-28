02cb94a34ffeed27c4f1e6de2773fd40fa43db69
Paul-Ndirangu <paulmwaura254@gmail.com>
2024-01-28 18:29:01 +0300

    tests commit

--- todo-app/todo/tests.py
+++ todo-app/todo/tests.py
@@ -1,5 +1,12 @@
-from django.test import TestCase
+from contextlib import AbstractContextManager
 import datetime
+from typing import Any
+
+from django.db.models import QuerySet
+from django.http import HttpResponse
+from django.urls import reverse
+from django.test import TestCase, Client
+
 from todo.models import Task
 
 # App tests.
@@ -35,3 +42,39 @@ class TestTaskModel(TestCase):
         actual = str(self.task)
         
         self.assertEqual(expected, actual)
+
+
+class TestIndexView(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.url = reverse("index")
+        cls.client = Client()
+        cls.task = Task.objects.create(name="Do a Python project")
+        
+    def test_index_view_returns_httpresponse(self):
+        """Test our view returns a HttpResponse"""
+        
+        response = self.client.get(self.url)
+        
+        self.assertTrue(isinstance(response, HttpResponse))
+        
+    def test_status_code(self):
+        response = self.client.get(self.url)
+        
+        self.assertEqual(response.status_code, 200)
+        
+    def test_context(self):
+        """Tests the context contains queryset of tasks"""
+        response = self.client.get(self.url)
+        
+        self.assertIn("tasks", response.context)
+        
+        tasks = response.context["tasks"]
+        
+        self.assertTrue(isinstance(tasks, QuerySet))
+        self.assertEqual(tasks.first(), self.task)
+        
+    def test_template_used(self):
+        response = self.client.get(self.url)    
+        self.assertTemplateUsed(response, "temp_index.html")
+        
