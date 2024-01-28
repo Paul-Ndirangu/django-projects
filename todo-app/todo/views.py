afe06908ed14d5eca9f99bef6eeb3cb86e4d2d45
Paul-Ndirangu <paulmwaura254@gmail.com>
2024-01-28 17:53:19 +0300

    .

--- todo-app/todo/views.py
+++ todo-app/todo/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
+from todo.models import Task
 
-# Create your views here.
+# Views.
+
+def index(request: HttpRequest) -> HttpResponse:
+    tasks = Task.objects.all().order_by("-created")
+
+    context = {"tasks": tasks}
+    
+    return render(request, "temp_index.html", context)
+    
