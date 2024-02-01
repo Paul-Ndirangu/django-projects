5f5d0c175731bdfbc16293f755fd0fbefedf9835
Paul-Ndirangu <paulmwaura254@gmail.com>
2024-02-01 22:47:41 +0300

    .

--- todo-app/todo/views.py
+++ todo-app/todo/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
+from todo.forms import TaskForm
 from todo.models import Task
 
 # Views.
 
 def index(request):
+    form = TaskForm()
     tasks = Task.objects.all().order_by("-created")
 
     context = {"tasks": tasks}
