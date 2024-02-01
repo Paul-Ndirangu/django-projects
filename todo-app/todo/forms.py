795c2f0d684b8eae0992801e93589441209cc66d
Paul-Ndirangu <paulmwaura254@gmail.com>
2024-02-01 23:50:49 +0300

    .

--- todo-app/todo/forms.py
+++ todo-app/todo/forms.py
@@ -8,3 +8,13 @@ class TaskForm(forms.ModelForm):
         model = Task
         fields = ["name"]
         
+    name = forms.CharField(
+        label=False,
+        widget=forms.TextInput(
+            attrs={
+                "aria-label": "name",
+                "placeholder": "What do you need to do?",
+                "class": "form-control form-control-lg inline-block",
+            }
+        )
+    )
