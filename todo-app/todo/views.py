eb24b8fb1813932b18d4258dca73a74759af428e
Paul-Ndirangu <paulmwaura254@gmail.com>
2024-02-06 17:23:25 +0300

    update task status

--- todo-app/todo/views.py
+++ todo-app/todo/views.py
@@ -1,7 +1,8 @@
 # todo/views.py
  
-from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse, HttpResponseBadRequest, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
  
 from todo.forms import TaskForm
 from todo.models import Task
@@ -24,3 +25,18 @@ def index(request: HttpRequest) -> HttpResponse:
         }
  
     return render(request, "index.html", context)
+
+def update_task_status(
+    request: HttpRequest, task_id: int, new_status: str 
+) -> HttpResponse:
+    if not new_status in Task.StatusChoice.values:
+        return HttpResponseBadRequest("Invalid status")
+    
+    task = get_object_or_404(Task, id=task_id)
+    
+    task.status = new_status
+    task.save()
+    
+    success_url = reverse("index")
+    
+    return HttpResponseRedirect(success_url)        
