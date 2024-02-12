ea26e6ca7bb2bc216e269292bb0953c3ed6be0ba
Paul-Ndirangu <paulmwaura254@gmail.com>
2024-02-12 19:29:46 +0300

    .

--- weather-app/weatherapp/views.py
+++ weather-app/weatherapp/views.py
@@ -1,4 +1,20 @@
 from django.shortcuts import render
+import requests
+
 
 def index(request):
-    return render(request, 'weatherapp/index.html')
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=6da8a860863f6aff086783e3a36b3c08'
+    city = 'Las Vegas'
+    city_weather = requests.get(url.format(city)).json() #request the API data and convert the JSON to Python data types
+    
+    weather = {
+    'city' : city,
+    'temperature' : city_weather['main']['temp'],
+    'description' : city_weather['weather'][0]['description'],
+    'icon' : city_weather['weather'][0]['icon']
+    }
+    
+    context = {'weather' : weather}
+    
+    return render(request, 'weather/index.html', context)
+
