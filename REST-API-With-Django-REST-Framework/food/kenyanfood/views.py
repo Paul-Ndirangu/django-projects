6124bb1bd2b9351085c805ecda801b6c6e9f18ed
Paul-Ndirangu <paulmwaura254@gmail.com>
2024-02-21 00:17:03 +0300

    .

--- REST-API-With-Django-REST-Framework/food/kenyanfood/views.py
+++ REST-API-With-Django-REST-Framework/food/kenyanfood/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from .models import Food
+from .serializer import FoodSerializer
 
 
 @api_view(['GET'])
 def getFood(request):
-    return  Response()
+    food = Food.objects.all()
+    serializer = FoodSerializer(food, many=True)
+    return  Response(serializer.data)
 
 @api_view(['POST'])
 def postFood(request):
